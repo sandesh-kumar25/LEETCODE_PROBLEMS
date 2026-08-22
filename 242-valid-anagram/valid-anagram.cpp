af4a5726329835a300cb6a0d0ef6896e7f8ea300
sandesh-kumar25 <sandeshkumar8961@gmail.com>
2026-08-22 21:26:22 +0530

    Time: 2 ms (49.80%) | Memory: 9.6 MB (75.85%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        if(s==t){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
+
+/*Also use O(nlogn)
+like:
+sort(s);
+sort(t);
+if(s==t){
+return true;
+}
+else{
+return false;
+}*/
