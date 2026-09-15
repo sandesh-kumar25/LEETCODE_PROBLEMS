ba1acd9d5aaed0485d81155fe70873ad59c5966b
sandesh-kumar25 <sandeshkumar8961@gmail.com>
2026-09-15 22:49:45 +0530

    Time: 5 ms (25.12%) | Memory: 73.6 MB (6.87%) - LeetSync

---
+++ 135-candy/candy.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n=ratings.size();
+        vector<int>left(n,1);
+        vector<int>right(n,1);
+
+        for(int i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1]){
+                left[i]=left[i-1]+1;  //current rating previous se zyada hai, toh current ko previous se 1 candy zyada.
+            }
+        }
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]){
+                right[i]=right[i+1]+1;
+            }
+        }
+        int total=0;
+        for(int i=0;i<n;i++){
+            total+=max(right[i],left[i]);
+        }
+        return total;
+    }
+};
